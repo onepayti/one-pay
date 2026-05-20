343cbfa406eb5385acf3debd396ddad7a0f7cb98
GabrielVieira21 <gabriel.vieira@1pay.com.br>
2026-05-20 11:04:22 -0300

    add tag v3

--- includes/header.php
+++ includes/header.php
@@ -47,11 +47,11 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1241497181154167');
+fbq('init', '1278911654383154');
 fbq('track', 'PageView');
 </script>
 <noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1241497181154167&ev=PageView&noscript=1"
+src="https://www.facebook.com/tr?id=1278911654383154&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Meta Pixel Code -->
 
